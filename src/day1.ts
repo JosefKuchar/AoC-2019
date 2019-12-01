72bcbaddaa0436808770c8562d37ada47fa331bc
Josef Kuchař <josef.kuchar267@gmail.com>
2019-12-01 09:52:16 +0100

    Format

--- src/day1.ts
+++ src/day1.ts
@@ -2,10 +2,10 @@ function calculateFuel(mass: number): number {
   let sum = 0;
   let fuel = Math.floor(mass / 3) - 2
   if (fuel > 0) {
-    sum += fuel;
-    sum += calculateFuel(fuel);
+    sum += fuel
+    sum += calculateFuel(fuel)
   }
-  return sum;
+  return sum
 }
 
 export function part1(masses: number[]) {
@@ -13,7 +13,7 @@ export function part1(masses: number[]) {
 }
 
 export function part2(masses: number[]) {
-  return masses.reduce((acc, fuel) => calculateFuel(fuel) + acc, 0);
+  return masses.reduce((acc, fuel) => calculateFuel(fuel) + acc, 0)
 }
 
 export function solve(input: string) {
