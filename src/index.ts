a88446f011238436a9cfb054d8ca43a8e7033f85
Josef Kuchař <josef.kuchar267@gmail.com>
2019-12-05 18:22:34 +0100

    Day 5

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,14 @@ import * as day1 from './day1';
 import * as day2 from './day2';
 import * as day3 from './day3';
 import * as day4 from './day4';
+import * as day5 from './day5';
 
 const days = [
     day1,
     day2,
     day3,
     day4,
+    day5,
 ];
 
 const day = parseInt(process.argv[2]);
